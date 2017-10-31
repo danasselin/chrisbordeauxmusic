5626932008864176fe7911bc91f4a0dc3fcd015f
danasselin <dasselin86@gmail.com>
2017-10-30 21:41:13 -0400

    Add fwd button to player

--- app/components/SongPlayerDisplay.jsx
+++ app/components/SongPlayerDisplay.jsx
@@ -3,10 +3,12 @@ import SongPlayerButton from './SongPlayerButton.jsx';
 import rewind from '../../public/SVG/rewind.svg';
 import play from '../../public/SVG/play.svg';
 import pause from '../../public/SVG/pause.svg';
+import forward from '../../public/SVG/forward.svg';
 
 const btnData = {
   rewind,
   play: { play, pause },
+  forward,
 };
 
 const SongPlayerDisplay = ({ btnOnClick }) => (
